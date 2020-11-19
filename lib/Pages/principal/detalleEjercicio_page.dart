030d63fbb34a59c4395cffb0fb5c5c0a47d262a9
Angelo Tapullima <angelo.anked@gmail.com>
2020-11-18 19:11:59 -0500

    la ayuda llego en camino

--- lib/Pages/principal/detalleEjercicio_page.dart
+++ lib/Pages/principal/detalleEjercicio_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:proyecto_codigo/models/ejerciciosAll.dart';
 import 'package:proyecto_codigo/models/resProfileRecovery_model.dart' as log;
-import 'package:proyecto_codigo/models/ejerciciosAll.dart' as general;
 import 'package:proyecto_codigo/utils/httphelper.dart';
 import 'package:proyecto_codigo/utils/preferences_user.dart';
 
@@ -45,45 +44,64 @@ class _DetalleEjercicioPageState extends State<DetalleEjercicioPage> {
     final EjerciciosAll exercises = ModalRoute.of(context).settings.arguments;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("${exercises.name}"),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: FutureBuilder(
-            future: HttpHelper().obtenerProgramaAll(id, token),
-            builder: (context, snapshot) {
-              List<EjerciciosAll> ejercicios = snapshot.data;
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    padding: EdgeInsets.all(0),
-                    itemCount: ejercicios.length,
-                    shrinkWrap: true,
-                    physics: ClampingScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return ListView.builder(
-                        itemCount: ejercicios[index].exercises.length,
-                        itemBuilder: (BuildContext context, int index2) {
-                          return Text("${ejercicios[index].exercises[index2].name}",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 24,
-                                  fontWeight: FontWeight.w800));
-                        },
-                      );
-                    });
-              } else if (snapshot.hasError) {
-                return Center(child: Text(snapshot.error.toString()));
-              } else {
-                return Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            },
-          ),
+        appBar: AppBar(
+          title: Text("${exercises.name}"),
         ),
-      ),
-    );
+        body: ListView.builder(
+            padding: EdgeInsets.all(0),
+            itemCount: exercises.exercises.length,
+            shrinkWrap: true,
+            physics: ClampingScrollPhysics(),
+            itemBuilder: (context, i) {
+              return Container(
+                height: 110,
+                width: double.infinity,
+                margin: EdgeInsets.only(right: 10, left: 10, bottom: 10,top: 10),
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image:NetworkImage(exercises.exercises[i].urlImage,),// AssetImage("assets/brazos.jpg"),
+                      //AssetImage("assets/brazos.jpg"),
+                      fit: BoxFit.cover,
+                      colorFilter:
+                          ColorFilter.mode(Colors.black54, BlendMode.darken),
+                    ),
+                    borderRadius: BorderRadius.circular(10)),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      exercises.exercises[i].name,
+                      style: TextStyle(
+                          color: Colors.white,
+                          //color: Theme.of(context).accentColor,
+                          fontSize: 24,
+                          fontWeight: FontWeight.w800),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      exercises.exercises[i].type,
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    SizedBox(height: 10),
+                  ],
+                ),
+              );
+
+              // return ListView.builder(
+              //   itemCount: ejercicios[index].exercises.length,
+              //   itemBuilder: (BuildContext context, int index2) {
+              //     return Text("${ejercicios[index].exercises[index2].name}",
+              //         style: TextStyle(
+              //             color: Colors.black,
+              //             fontSize: 24,
+              //             fontWeight: FontWeight.w800));
+              //   },
+              // );
+            }));
   }
 }
 
